d5b3b9d00a65a96a7a774babe61373a72dbaf814
Fabrice Valade <fabrice.valade@neoculus.com>
2019-07-24 12:40:09 -0400

    Initial version that can barely read file in core

--- PortraitEditor/MainWindow.xaml.cs
+++ PortraitEditor/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Ookii.Dialogs.Wpf;
+using System.Collections.ObjectModel;
+using System.Text.RegularExpressions;
 
 namespace PortraitEditor
 {
@@ -20,9 +25,58 @@ namespace PortraitEditor
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<FactionFile> CoreFaction { get; set; }
         public MainWindow()
         {
+            DataContext = this;
+            CoreFaction = new ObservableCollection<FactionFile>() { new FactionFile("ay\\yo\\hello.faction") };
             InitializeComponent();
         }
+        private void BtnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            //OpenFileDialog openFileDialog = new OpenFileDialog();
+            //openFileDialog.Filter = "faction files (*.faction)|*.faction";
+            //if (openFileDialog.ShowDialog() == true)
+            //    txtEditor.Text = File.ReadAllText(openFileDialog.FileName);
+            VistaFolderBrowserDialog OpenRootFolder = new VistaFolderBrowserDialog();
+            if (OpenRootFolder.ShowDialog() == true)
+            {
+                RootPathDisplay.Text = OpenRootFolder.SelectedPath;
+                UpdateFactionFileList(OpenRootFolder.SelectedPath);
+            }
+
+        }
+        private void UpdateFactionFileList(string path)
+        {   
+
+            DirectoryInfo CoreFactionDirectory = new DirectoryInfo(path+ "\\starsector-core\\data\\world\\factions");
+            IEnumerable<FileInfo> a = CoreFactionDirectory.EnumerateFiles();
+            foreach (FileInfo DataFile in a)
+            {
+
+                if (DataFile.Extension == ".faction")
+                    CoreFaction.Add(new FactionFile(DataFile.FullName));
+
+            }
+
+            //Data.Concat(RootDirectory.EnumerateFiles());
+
+            return;
+        }
     }
+    public class FactionFile
+    {
+        public string Path { get; set; }
+        public string FileId { get; set; }
+
+        public FactionFile() { }
+        public FactionFile(string newPath)
+        {
+            Path = newPath;
+            Regex ExtractFactionId = new Regex(@"(?:.*\\)(.*)(?:.faction)");
+            FileId = ExtractFactionId.Match(Path).Groups[1].ToString();
+        }
+
+    }
+    
 }
