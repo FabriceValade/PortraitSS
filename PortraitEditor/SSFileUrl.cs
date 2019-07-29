e7ed4eaf3e21be3039420e41a0cdc5126c2adab9
Fabrice Valade <fabrice_valade@hotmail.com>
2019-07-28 21:15:42 -0400

    just avnt de changer tout les string url a un object de classe

--- PortraitEditor/SSFileUrl.cs
+++ PortraitEditor/SSFileUrl.cs
@@ -11,15 +11,37 @@ namespace PortraitEditor
 {
     class SSFileUrl  : INotifyPropertyChanged
     {
+        //properties
         public event PropertyChangedEventHandler PropertyChanged;
         private string FullUrl { get { return this.FullUrl; } set { this.FullUrl = value; NotifyPropertyChanged(); } }
         private string RelativeUrl { get { return this.RelativeUrl; } set { this.RelativeUrl = value; NotifyPropertyChanged(); } }
+
+        //constructors
         public SSFileUrl(string url)
         {
             FullUrl = url;
             RelativeUrl = null;
         }
         public SSFileUrl(string SSurl,string relativeUrl)
+        {
+            FullUrl = Path.Combine(SSurl, relativeUrl);
+            RelativeUrl = relativeUrl;
+        }
+
+        //method
+        public bool IsAvailable()
+        {
+            if (File.Exists(FullUrl))
+                return true;
+            else
+                return false;
+        }
+        public void ChangeUrl(string newUrl)
+        {
+            FullUrl = newUrl;
+            RelativeUrl = null;
+        }
+        public void ChangeUrl(string SSurl,string relativeUrl)
         {
             FullUrl = Path.Combine(SSurl, relativeUrl);
             RelativeUrl = relativeUrl;
